41d563e4b620a5ddb2ce63c085bd3cfd2baac213
Vinicius Oliveira <viniciusjesus16@gmail.com>
2021-07-19 20:46:58 -0300

    Organizando e criando arquivos

---
+++ organization/src/components/loader.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/login.module.scss'
+
+export default function Loader() {
+  return (
+    <div className={styles.container}>
+      <img src="images/logo.webp"/>
+    </div>
+  )
+}
--- organization/src/pages/index.tsx
+++ organization/src/pages/index.tsx
@@ -1,10 +1,10 @@
-import styles from '../assets/styles/Login.module.scss'
+import styles from '../styles/Login.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       {/* <p>ORGANIZAÍ</p> */}
-      <img src="../assets/images/logo.webp"/>
+      <img src="images/logo.webp"/>
     </div>
   )
 }
